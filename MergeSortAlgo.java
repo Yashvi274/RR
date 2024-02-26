9d473e8a89f02693303bfc8b8b5607bbb6c763e2
Yashvi274 <sharmayashvi274@gmail.com>
2024-02-26 17:25:41 +0530

    s

--- MergeSortAlgo.java
+++ MergeSortAlgo.java
@@ -10,9 +10,7 @@ public class MergeSortAlgo {
         mergeSort(arr, mid+1, end);
         merge(arr, start,mid, end);
     }
-    public static void merge(int[] arr,int start,int mid,int end){
-        
-    }
+    
     public static void main(String[] args) {
         int arr[]={4,3,9,2,5,1,8};
         mergeSort(arr,0,arr.length-1);
